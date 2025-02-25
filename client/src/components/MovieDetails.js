3cd0f985240b6ec21b833e964794a5350c3d4c4e
ktenizo09 <ktenizo09@gmail.com>
2025-02-25 04:25:03 +0000

    Navigation added + Single Movie Details working

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
-import NavLink from "react-bootstrap/esm/NavLink";
+import { useParams, useNavigate } from 'react-router-dom';
 
 const MovieDetailsPage = () => {
     const { movieId } = useParams();
     const [movie, setMovie] = useState(null);
+    const navigate = useNavigate();
 
-   
 
     const getMovieDetails = async () => {
         try {
@@ -22,28 +21,40 @@ const MovieDetailsPage = () => {
     }
 
     useEffect(() => {
+
         getMovieDetails()
-    })
-    
+
+    }, [movieId]);
+
+
+
+    const handleGoBack = () => {
+
+        console.log("Go back function triggered");
+        navigate(-1);  // Goes back to the previous page in the history stack
+    };
     return (
-        <section className="movie-section">
-            <div className="movie-card">
-                <figure>
-                    <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="movie poster" />
-                </figure>
-                <div className="card-content">
-                    <p className="title">{movie.title}</p>
-                    <p className="tagline">{movie.tagline}</p>
-                    <p className="overview">{movie.overview}</p>
-                    <p className="card-text">{movie.release_date}</p>
-                    <p className="card-text">{movie.genre}</p>
-                    <p className="card-text">{movie.vote_average} / 10</p>
-                    <NavLink to="../" className="back-btn">
-                        Go Back
-                    </NavLink>
-                </div>
-            </div>
-        </section>
+        <div>
+            {(typeof movie === 'undefined' || movie === null) ?
+                (<p>Loading...</p>) :
+                (<section className="movie-section">
+                    <div className="movie-card">
+                        <figure>
+                            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="movie poster" />
+                        </figure>
+                        <div className="card-content">
+                            <p className="title">{movie.title}</p>
+                            <p className="tagline">{movie.tagline}</p>
+                            <p className="overview">{movie.overview}</p>
+                            <p className="card-text">{movie.release_date}</p>
+                            <p className="card-text">{movie.genre}</p>
+                            <p className="card-text">{movie.vote_average} / 10</p>
+                            <button onClick={handleGoBack} className="btn btn-primary" >Back to Movies</button>
+                        </div>
+                    </div>
+                </section>)
+            }
+        </div>
     );
 }
 
